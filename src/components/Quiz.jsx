fe98c575c1e060d943461d530e68844d2cb61e30
KevinSmhevin <KevinSmhevin@users.noreply.github.com>
2025-08-25 11:59:42 -0700

    start-quiz

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,8 @@
+export default function Quiz() {
+  return (
+    <div className="quiz-container">
+      <h2 className="quiz-title">Quiz Time!</h2>
+      {/* Quiz content will go here */}
+    </div>
+  )
+}
---
+++ src/components/Start.jsx
@@ -0,0 +1,9 @@
+export default function Start(props) {
+  return (
+    <header className="start-container">
+      <h1 className="start-title">Quizzical</h1>
+      <p className="start-subtitle">Test your knowledge with fun quizzes!</p>
+      <button className="start-button" onClick={props.onStart}>Start Quiz</button>
+    </header>
+  )
+}
